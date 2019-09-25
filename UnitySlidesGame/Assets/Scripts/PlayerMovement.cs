2282fdd844c65f0d8d1a2c83892dcec0f4566ae7
Shiva Teja Chigicherla <shiva000teja@gmail.com>
2019-09-25 23:43:36 +0530

    Day02

---
+++ UnitySlidesGame/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;     // adding a rigid body rb
+    public float ForwardForce = 2000f;
+    public float SidewaysForce = 500f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello World");
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(0,0,ForwardForce*Time.deltaTime);
+
+        if(Input.GetKey("d")){
+            rb.AddForce(SidewaysForce*Time.deltaTime,0,0,ForceMode.VelocityChange);
+        }
+        if(Input.GetKey("a")){
+            rb.AddForce(-SidewaysForce*Time.deltaTime,0,0,ForceMode.VelocityChange);
+        }
+    }
+}
